da7a6ab5754976d1d2b9e58f1a18e292870343a5
didya <2198005977@qq.com>
2026-01-04 09:27:11 +0800

    GateWay改名，pom简化

--- shiwujie-backend/shiwujie-gateway/src/main/java/com/swj/shiwujie/GatewayApplication.java
+++ shiwujie-backend/shiwujie-gateway/src/main/java/com/swj/shiwujie/GatewayApplication.java
@@ -3,14 +3,16 @@ package com.swj.shiwujie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Slf4j
-public class GatewatyApplication {
+public class GatewayApplication {
 
     public static void main(String[] args)  {
-        SpringApplication.run(GatewatyApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
 
     }
 
